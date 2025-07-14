8f0898b0425eab4aefa8637a803df075c9df0f39
Marco Maisel <info@marcomaisel.de>
2025-07-14 11:58:01 +0200

    upgrade to tailwind 4 and nuxt compatibilityVersion 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4,
+  },
   modules: ['@nuxtjs/tailwindcss', 'nuxt-purgecss'],
   css: ['@/assets/styles/main.scss'],
   vite: {
